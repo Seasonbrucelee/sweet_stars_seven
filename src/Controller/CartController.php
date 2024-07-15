9d5d11194894a4d2e7eb032152111d6c05f219dc
Seasonbrucelee <sezen39@hotmail.com>
2024-07-15 22:41:07 +0200

    gestion panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,29 +2,48 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
+use SessionIdInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'page_cart')]
-    public function cart(): Response
-    {
-        return $this->render('cart/cart.html.twig', [
-            'controller_name' => 'CartController',
+    public function cart(SessionInterface $session, ProductRepository $productRepository): Response
+    {   
+    $cart = $session->get('cart', []);
+
+    $cartWithData = [];
+    foreach($cart as $id => $quantity){
+        $cartWithData[] = [
+            'product' => $productRepository->find($id),
+            'quantity' => $quantity
+        ];
+    }   
+    //dd($cartWithData);
+        return $this->render('cart/cart.html.twig', [   
+            'items' => $cartWithData
         ]);
     }
-    #[Route("/cart/add/{id}", name:"page_cart_add")]
-    public function add($id, Request $request) {
-            $session = $request->getSession();
+
+    #[Route('/cart/add/{id}', name:'cart_add')]
+
+    public function add($id, SessionInterface $session) {
+            //$session = $request->getSession();
             
             $cart = $session->get('cart', []);
-
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        }else{
             $cart[$id] = 1;
-
+            }
             $session->set('cart', $cart);
+            return $this->redirectToRoute('page_cart');
             dd($session->get('cart'));
 
     }
